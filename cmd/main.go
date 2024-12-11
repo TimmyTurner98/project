be5108ea0a6a90dc3d7d1e79d7ef8eb6a029cb89
TimmyTurner <suleimenovt.98@gmail.com>
2024-12-11 15:03:25 +0500

    test

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/TimmyTurner98/project"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	srv := new(project.Server)
+
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			logrus.Fatalf("error occured while running http server: %s", err.Error())
+		}
+	}()
+
+	logrus.Print("Project Started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	logrus.Print("Project Shutting Down")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Errorf("error occured on server shutting down: %s", err.Error())
+	}
+
+	if err := db.Close(); err != nil {
+		logrus.Errorf("error occured on db connection close: %s", err.Error())
+	}
+}
---
+++ pkg/handler/handler.go
@@ -0,0 +1,13 @@
+package handler
+
+import (
+	"github.com/TimmyTurner98/project/pkg/service"
+)
+
+type Handler struct {
+	services *service.Service
+}
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
+}
